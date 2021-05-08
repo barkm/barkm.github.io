77f73f3f22de1edc7ce29df388979c842b98de1e
Patrik Barkman <patrik.barkman@gmail.com>
2021-05-08 11:15:21 +0200

    handle refraction in fragment shader

--- src/shaders/surface/face/fragment.glsl
+++ src/shaders/surface/face/fragment.glsl
@@ -1,14 +1,23 @@
 uniform vec3 uSeaColor;
 uniform vec3 uSkyColor;
+uniform float uIndicesOfRefractionRatio;
 
 varying float vVisibility;
-varying vec3 vRefraction;
+varying vec3 vModelPosition;
+
+vec3 getRefaction() {
+    float dYDisplacementDx = dFdx(vModelPosition.y);
+    float dYDisplacementDz = dFdy(vModelPosition.y);
+    vec3 tangentX = vec3(1.0, dYDisplacementDx, 0.0);
+    vec3 tangentZ = vec3(0.0, dYDisplacementDz, 1.0);
+    vec3 normal = normalize(cross(tangentX, tangentZ));
+    vec3 incident = normalize(vModelPosition - cameraPosition);
+    return refract(incident, normal, uIndicesOfRefractionRatio);
+}
 
 void main() {
-    vec3 refraction = vRefraction;
-    float totalReflection = 1.0 - length(refraction);
-
-    vec3 color = mix(uSkyColor, uSeaColor, totalReflection);
+    vec3 refraction = getRefaction();
+    vec3 color = mix(uSeaColor, uSkyColor, refraction.y);
     color = mix(uSeaColor, color, vVisibility);
     gl_FragColor = vec4(color, 1.0);
 }
--- src/shaders/surface/face/vertex.glsl
+++ src/shaders/surface/face/vertex.glsl
@@ -3,46 +3,29 @@
 uniform float uTime;
 uniform float uMinVisibility;
 uniform float uMaxVisibility;
-uniform float uIndicesOfRefractionRatio;
 
 varying float vVisibility;
-varying vec3 vRefraction;
+varying float vDisplacement;
+varying vec3 vModelPosition;
 
 float getVisibility(const vec3 pos) {
   float distanceFromCamera = distance(pos, cameraPosition);
   return 1.0 - fogLinear(distanceFromCamera, uMinVisibility, uMaxVisibility);
 }
 
-float yDisplacement(float x, float z, float t) {
+float getYDisplacement(float x, float z, float t) {
     return 0.1 * (sin(x - 0.5 * t) + cos(z - 0.5 * t));
-}
-
-float dYDisplacementDx(float x, float z, float t) {
-    return 0.1 * cos(x - 0.5 * t);
-}
-
-float dYDisplacementDz(float x, float z, float t) {
-    return -0.1 * sin(z - 0.5 * t);
-}
-
-vec3 yDisplacementNormal(float x, float z, float t) {
-  vec3 tangentX = vec3(1.0, dYDisplacementDx(x, z, t), 0.0);
-  vec3 tangentZ = vec3(0.0, dYDisplacementDz(x, z, t), 1.0);
-  return normalize(cross(tangentX, tangentZ));
 }
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.y += yDisplacement(modelPosition.x, modelPosition.z, uTime);
+    modelPosition.y += getYDisplacement(modelPosition.x, modelPosition.z, uTime);
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
-    vec3 indicent = normalize(modelPosition.xyz - cameraPosition);
-    vec3 normal = yDisplacementNormal(modelPosition.x, modelPosition.z, uTime);
-
     gl_Position = projectedPosition;
     vVisibility = getVisibility(modelPosition.xyz);
-    vRefraction = refract(indicent, normal, uIndicesOfRefractionRatio);
+    vModelPosition = modelPosition.xyz;
 }
