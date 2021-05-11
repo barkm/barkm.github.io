370d82ab05582be9658e6fe1b054722a3b4b1d87
Patrik Barkman <patrik.barkman@gmail.com>
2021-05-11 08:12:37 +0200

    refactor barycentric

--- src/ts/three/barycentric.ts
+++ src/ts/three/barycentric.ts
@@ -1,36 +1,49 @@
 import * as THREE from "three";
 
+type Assignment = 0 | 1 | 2;
+
 function getBarycentricCoordinates(
-  geometry: THREE.BufferGeometry
-): Float32Array | null {
-  const positionAttribute = geometry.attributes.position;
-  const vertexFromFace = geometry.getIndex();
-  if (!vertexFromFace || vertexFromFace.count != positionAttribute.count) {
-    console.log("Could not get barycentric coordinates");
-    return null;
-  }
-  const barycentricCoordinates = new Float32Array(positionAttribute.count * 3);
+  asssignents: Array<Assignment>
+): Float32Array {
+  const barycentricCoordinates = new Float32Array(asssignents.length * 3);
   const corners = [
     new THREE.Vector3(1, 0, 0),
     new THREE.Vector3(0, 1, 0),
     new THREE.Vector3(0, 0, 1),
   ];
-  for (let i = 0; i < positionAttribute.count; i++) {
-    if (vertexFromFace) {
-      const vertex = vertexFromFace.array[i];
-      corners[i % 3].toArray(barycentricCoordinates, vertex * 3);
-    }
+  for (let i = 0; i < asssignents.length; i++) {
+    corners[asssignents[i]].toArray(barycentricCoordinates, i * 3);
   }
   return barycentricCoordinates;
+}
+
+function getIndependentTrianglesAssignments(
+  geometry: THREE.BufferGeometry
+): Array<Assignment> | null {
+  const vertexFromFaceAttribute = geometry.getIndex();
+  if (
+    !vertexFromFaceAttribute ||
+    geometry.getAttribute("position").count == vertexFromFaceAttribute.count * 3
+  ) {
+    console.log("Not able to get assignments from geometry");
+    return null;
+  }
+  const vertexFromFace = vertexFromFaceAttribute.array;
+  const assignments: Array<Assignment> = new Array(vertexFromFace.length);
+  for (let i = 0; i < vertexFromFace.length; i++) {
+    assignments[vertexFromFace[i]] = (i % 3) as Assignment;
+  }
+  return assignments;
 }
 
 export function setBarycentricCoordinateAttribute(
   geometry: THREE.BufferGeometry
 ): void {
-  const barycentricCoordinates = getBarycentricCoordinates(geometry);
-  if (!barycentricCoordinates) {
+  const assignments = getIndependentTrianglesAssignments(geometry);
+  if (!assignments) {
     return;
   }
+  const barycentricCoordinates = getBarycentricCoordinates(assignments);
   geometry.setAttribute(
     "aBarycentricCoordinate",
     new THREE.BufferAttribute(barycentricCoordinates, 3)
